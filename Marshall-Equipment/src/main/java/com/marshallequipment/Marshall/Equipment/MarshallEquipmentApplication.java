30348257515c0b5fef90da6e77c5988a80925b6b
Luis <flimon04567@gmail.com>
2022-11-28 20:06:02 -0600

    construct java-spring proyect

---
+++ Marshall-Equipment/src/main/java/com/marshallequipment/Marshall/Equipment/MarshallEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.marshallequipment.Marshall.Equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshallEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarshallEquipmentApplication.class, args);
+	}
+
+}
---
+++ Marshall-Equipment/src/test/java/com/marshallequipment/Marshall/Equipment/MarshallEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marshallequipment.Marshall.Equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarshallEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
